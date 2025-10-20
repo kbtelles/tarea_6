1b8e5c0357e0113b3c9be969510a4d7a0706e6c7
kbtelles <kbtelles@scl.edu.gt>
2025-10-19 20:32:12 -0600

    subiendo tarea

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using ApiEmpresa.Models;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<Conexiones>(opt =>
+opt.UseMySQL(builder.Configuration.GetConnectionString("cnSqlServer") ));
+// opt.UseSqlServer(builder.Configuration.GetConnectionString("cnSqlServer") )); es para sql server
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+     app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
