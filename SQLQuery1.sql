efa6b242121885da584c8ef78f6231097ea71cc0
kbtelles <kbtelles@scl.edu.gt>
2025-10-19 20:34:09 -0600

    subiendo tarea

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+INSERT INTO [db_empresa].[dbo].[Cliente] 
+([nombres], [apellidos], [direcccion], [telefono], [fecha_nacimiento])
+VALUES
+('María Fernanda', 'López Ramírez', 'Zona 10, Ciudad de Guatemala', '50212345678', '1998-04-15'),
+('Carlos Eduardo', 'Gómez Hernández', 'Colonia El Prado, Mixco', '50245678912', '1989-09-27'),
+('Andrea Paola', 'Martínez Castillo', 'Barrio San Antonio, Quetzaltenango', '50258741236', '2001-02-08'),
+('Luis Alberto', 'Pérez Juárez', 'Zona 3, Cobán, Alta Verapaz', '50233659874', '1995-11-20'),
+('Sofía Daniela', 'Chacón Méndez', 'Colonia San Cristóbal, Mixco', '50267891234', '2000-07-03'),
+('José Manuel', 'Rodríguez Ortiz', 'Zona 5, Ciudad de Guatemala', '50241236985', '1987-03-18'),
+('Karla Beatriz', 'Hernández López', 'Barrio El Calvario, Chiquimula', '50269874512', '1993-12-29'),
+('Diego Alejandro', 'Morales Cabrera', 'Zona 1, Escuintla', '50254789631', '1999-06-22'),
+('Valeria Sofía', 'Ruiz López', 'Aldea El Progreso, Jutiapa', '50247891256', '2002-01-11'),
+('Ricardo Antonio', 'Cordero García', 'Zona 2, Santa Lucía Cotzumalguapa', '50236987415', '1991-09-05');
